f52f5158a2dc10416c266f13023ae8bf90d4eda9
Adem Gaygusuz <adem@ardweb.co.uk>
2016-10-12 23:20:08 +0100

    Fix task recursion bug

--- service/features/signals.py
+++ service/features/signals.py
@@ -6,6 +6,6 @@ from .tasks import import_features_from_git_repo
 
 
 @receiver(post_save, sender=Project)
-def handle_new_project(sender, instance, **kwargs):
-    if instance.git_repo_url:
+def handle_new_project(sender, instance, created, **kwargs):
+    if created and instance.git_repo_url:
         import_features_from_git_repo.delay(instance.id)
