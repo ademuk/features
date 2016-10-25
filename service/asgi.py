caa6450706a5cb6ef9f35c11ff67ab4607073c06
Adem Gaygusuz <adem@ardweb.co.uk>
2016-10-25 23:02:28 +0100

    Remove service Move client back to root

--- service/asgi.py
+++
@@ -1,8 +0,0 @@
-# asgi.py
-
-import os
-from channels.asgi import get_channel_layer
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service.settings")
-
-channel_layer = get_channel_layer()
--- service/routing.py
+++
@@ -1,9 +0,0 @@
-from channels import route
-from .features import consumers
-
-path = r'^/api/projects/(?P<pk>\d+)/stream/$'
-
-channel_routing = [
-    route("websocket.connect", consumers.connect_to_project, path=path),
-    route("websocket.receive", consumers.disconnect_from_project, path=path)
-]
