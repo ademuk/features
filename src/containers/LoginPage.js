7cdacc222cfbfc9d7bcbf90b0b9f99cfeac0a54a
Adem Gaygusuz <adem@ardweb.co.uk>
2017-02-16 23:14:59 +0000

    Switch Login to use formsyFIeld

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,10 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Input } from 'semantic-ui-react';
 import Formsy from 'formsy-react';
 
-import { FormsyInput } from '../formsy-semantic-ui';
+import { FormsyField } from '../formsy-semantic-ui';
 import { createSession } from '../actions/auth';
 
 class LoginPage extends Component {
@@ -20,8 +20,8 @@ class LoginPage extends Component {
     return (
       <Form as={Formsy.Form} onValidSubmit={this.handleSubmit}>
         <h2>Login</h2>
-        <FormsyInput name="username" label="Username" placeholder="amigo" required />
-        <FormsyInput name="password" label="Password" placeholder="B33d33d33" type="password" required />
+        <FormsyField control={Input} name="username" label="Username" placeholder="amigo" required />
+        <FormsyField control={Input} name="password" label="Password" placeholder="B33d33d33" type="password" required />
         <Button type="submit" color="teal">Log-in</Button>
       </Form>
     );
