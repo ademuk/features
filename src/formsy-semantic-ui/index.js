41e43c04724b1a6bd17cd7c6908e07a7a6e3829e
Adem Gaygusuz <adem@ardweb.co.uk>
2017-02-16 23:14:09 +0000

    Revert "Switch login to use FormsyField"
    This reverts commit 22089b45922c9726d6ba48239d8b7c2cd60bdf01.

---
+++ src/formsy-semantic-ui/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Formsy from 'formsy-react';
+import { Form, Input } from 'semantic-ui-react';
+
+
+export const FormsyField = React.createClass({
+
+  mixins: [Formsy.Mixin],
+
+  getInitialState() {
+    return { value: this.controlledValue() };
+  },
+
+  controlledValue(props = this.props) {
+    return props.value || '';
+  },
+
+  changeValue(event) {
+    this.setState({ value: event.currentTarget.value });
+    this.setValue(event.currentTarget.value);
+  },
+
+  render() {
+    const {
+      validationError,
+      validationErrors,
+      ...nonFormsyProps
+    } = this.props;
+
+    return (
+      <Form.Field {...nonFormsyProps}
+                  value={this.state.value}
+                  onChange={this.changeValue}
+                  required={this.isRequired()}
+                  error={this.isFormSubmitted() && this.showRequired()}>
+      </Form.Field>
+    );
+  }
+});
+
+
+export const FormsyInput = React.createClass({
+
+  mixins: [Formsy.Mixin],
+
+  getInitialState() {
+    return { value: this.controlledValue() };
+  },
+
+  controlledValue(props = this.props) {
+    return props.value || '';
+  },
+
+  changeValue(event) {
+    this.setState({ value: event.currentTarget.value });
+    this.setValue(event.currentTarget.value);
+  },
+
+  render() {
+    const {
+      validationError,
+      validationErrors,
+      ...nonFormsyProps
+    } = this.props;
+
+    return (
+      <Form.Input {...nonFormsyProps}
+                  value={this.state.value}
+                  required={this.isRequired()}
+                  error={this.isFormSubmitted() && this.showRequired()}
+                  onChange={this.changeValue} />
+    );
+  }
+});
+
